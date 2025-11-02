b6bda5857588cfecaf93ea595757a78e30088fe9
zkhan907557-oss <zkhan907557@gmail.com>
2025-11-02 09:03:18 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-    base:process.env.VITE_BASE_PATH  || "/Resume-Wiz",
-   },
-}));
+export default defineConfig({
+  plugins: [react()],
+})
